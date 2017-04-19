d5d5b6cf9bbd5db6128380330d01bd04732e5a45
WAW Developer <wawdeveloper@gmail.com>
2017-04-19 09:59:35 +0200

    Provision files updated.
    - Better location for nodejs link to system path.

--- provision/setup_bower.sh
+++ provision/setup_bower.sh
@@ -2,4 +2,3 @@
 
 echo "Installing bower"
 sudo npm install -g bower
-sudo ln -s /usr/bin/nodejs /usr/bin/node
--- provision/setup_nodejs.sh
+++ provision/setup_nodejs.sh
@@ -4,3 +4,4 @@ echo "Installing nodejs"
 curl -sL https://deb.nodesource.com/setup | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
+sudo ln -s /usr/bin/nodejs /usr/bin/node
